05cfc2332206e2f78a39cb4c982db5d343cc2e9d
李富 <lifuzz@163.com>
2015-05-05 13:26:12 +0800

    定位搞定了

--- app/src/main/java/com/prd/yun/MainActivity.java
+++ app/src/main/java/com/prd/yun/MainActivity.java
@@ -2,14 +2,41 @@ package com.prd.yun;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration.LocationMode;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.model.LatLng;
+import com.baidu.mapapi.search.geocode.GeoCodeResult;
+import com.baidu.mapapi.search.geocode.GeoCoder;
+import com.baidu.mapapi.search.geocode.OnGetGeoCoderResultListener;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeOption;
+import com.baidu.mapapi.search.geocode.ReverseGeoCodeResult;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    MapView mMapView = null;
+    private MapView mMapView = null;
+    private BaiduMap mBaiduMap;
+    // 定位相关
+    LocationClient mLocClient;
+    public MyLocationListenner myListener = new MyLocationListenner();
+    private LocationMode mCurrentMode;
+    BitmapDescriptor mCurrentMarker;
+    boolean isFirstLoc = true;// 是否首次定位
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,19 +46,88 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         //获取地图控件引用
         mMapView = (MapView) findViewById(R.id.bmapView);
+
+
+        mCurrentMode = LocationMode.NORMAL;
+        // 地图初始化
+
+        mBaiduMap = mMapView.getMap();
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+
+        // 定位初始化
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);// 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        option.setAddrType("all");
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+
+
     }
+
+
+    public class MyLocationListenner implements BDLocationListener {
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            if (location == null || mMapView == null) {
+                return;
+            }
+
+            MyLocationData locationData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locationData);
+
+
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+                mBaiduMap.animateMapStatus(u);
+                Log.i("tag",ll.latitude +"  " +ll.longitude);
+
+                OverlayOptions option = new MarkerOptions().position(ll).icon(mCurrentMarker);
+                mBaiduMap.addOverlay(option);
+
+                GeoCoder getCoder = GeoCoder.newInstance();
+                ReverseGeoCodeOption reCodeOption = new ReverseGeoCodeOption();
+                reCodeOption.location(ll);
+                getCoder.reverseGeoCode(reCodeOption);
+                getCoder.setOnGetGeoCodeResultListener(new OnGetGeoCoderResultListener() {
+                    @Override
+                    public void onGetGeoCodeResult(GeoCodeResult geoCodeResult) {
+                        Log.i("tag",geoCodeResult.getAddress()+":onGetGeoCodeResult");
+                    }
+
+                    @Override
+                    public void onGetReverseGeoCodeResult(ReverseGeoCodeResult reverseGeoCodeResult) {
+
+                        Log.i("tag",reverseGeoCodeResult.getAddress()+":reverseGeoCodeResult");
+                    }
+                });
+            }
+        }
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
         mMapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
